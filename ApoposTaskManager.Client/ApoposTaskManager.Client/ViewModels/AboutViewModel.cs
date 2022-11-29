4fd316adca48d47eb343b9ae327011243c37d988
2Xpro-pop <bay.asanov@mail.ru>
2022-11-29 22:29:10 +0600

    ReactiveUI and ReactiveUI are installed.Fodi
    Also demonstrated how it works(AboutPage & AboutViewModel)

--- ApoposTaskManager.Client/ApoposTaskManager.Client/ViewModels/AboutViewModel.cs
+++ ApoposTaskManager.Client/ApoposTaskManager.Client/ViewModels/AboutViewModel.cs
@@ -1,21 +1,46 @@
 ﻿using System;
+using System.Reactive.Linq;
 using System.Windows.Input;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ApoposTaskManager.Client.ViewModels
 {
-    public class AboutViewModel : BaseViewModel
+    public class AboutViewModel : ReactiveObject
     {
-        public AboutViewModel()
-        {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
-        }
+        [Reactive]
+        public int Counter { get; set; }
+        public ICommand IncrementCommand { get; }
 
-        public ICommand OpenWebCommand
+        [Reactive]
+        public string FirstName
         {
             get;
+            set;
+        }
+
+        [Reactive]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [ObservableAsProperty]
+        public string FullName
+        {
+            get;
+        }
+
+        public AboutViewModel()
+        {
+            IncrementCommand = ReactiveCommand.Create(() => Counter++);
+
+            this.WhenAnyValue(vm => vm.FirstName, vm => vm.LastName)
+                .Select(t => $"{t.Item1} {t.Item2}")
+                .ToPropertyEx(this, vm  => vm.FullName);
         }
     }
 }
--- ApoposTaskManager.Client/ApoposTaskManager.Client/Views/AboutPage.xaml.cs
+++ ApoposTaskManager.Client/ApoposTaskManager.Client/Views/AboutPage.xaml.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.ComponentModel;
+using ApoposTaskManager.Client.ViewModels;
+using ReactiveUI.XamForms;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ApoposTaskManager.Client.Views
 {
-    public partial class AboutPage : ContentPage
+    public partial class AboutPage : ReactiveContentPage<AboutViewModel>
     {
         public AboutPage()
         {
+            
             InitializeComponent();
         }
     }
